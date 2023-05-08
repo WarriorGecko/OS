dccf965a5422ee6330cba2d9cceec18da5f39f82
WarriorGecko <ioan.underwood@gmail.com>
2023-05-08 17:44:15 +0100

    Why am I here

---
+++ src/impl/x86_64/userInput.c
@@ -0,0 +1,13 @@
+#include "userInput.h"
+
+
+// void type(int lineLength){
+//     char line;
+//     int i;
+//     for (i = 0; i < lineLength; i++){
+//         line = getch();
+//         print_char(line);
+//     }
+
+    
+// }
---
+++ src/intf/userInput.h
@@ -0,0 +1,6 @@
+
+
+char* input(char* message, int length);
+void type(int lineLength);
+
+

a7f7092ea4416f86fdb7be6cb1d44029d6c79bb3
Ioan Underwood <ioan.underwood@gmail.com>
2023-12-06 11:07:57 +0000

    removed test code in kernel

--- src/impl/kernel/main.c
+++ src/impl/kernel/main.c
@@ -4,9 +4,7 @@
 
 
 void kernel_main() {
-    while (1 == 1){
-        print_clear();
-        print_set_color(PRINT_COLOR_GREEN, PRINT_COLOR_BLACK);
-        print_str("Welcome to Generation OS!");
-    }
+    print_clear();
+    print_set_color(PRINT_COLOR_GREEN, PRINT_COLOR_BLACK);
+    print_str("Welcome to Generation OS!");
 }

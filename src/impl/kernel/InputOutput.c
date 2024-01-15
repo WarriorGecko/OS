9dc9cf56dba81c3aaaf7faff0d9e7a327073ac86
WarriorGecko <ioan.underwood@gmail.com>
2024-01-15 12:45:47 +0000

    testing kernel

--- src/impl/kernel/InputOutput.c
+++
@@ -1,7 +0,0 @@
-extern port_byte_in;
-
-unsigned char portByteIn(unsigned short port){
-    unsigned char result;
-    result = port_byte_in;
-    
-};
--- src/impl/kernel/main.c
+++ src/impl/kernel/main.c
@@ -7,4 +7,5 @@ void kernel_main() {
     print_clear();
     print_set_color(PRINT_COLOR_GREEN, PRINT_COLOR_BLACK);
     print_str("Welcome to Generation OS!");
+    print_str("This OS is still in development");
 }

9069586dc929507bda50da9a8b6de548737a4904
WarriorGecko <ioan.underwood@gmail.com>
2024-01-16 14:29:01 +0000

    input woop

--- src/impl/kernel/main.c
+++ src/impl/kernel/main.c
@@ -1,11 +1,19 @@
-#include "print.h"
+    #include "print.h"
+    #include "io.h"
 
 
 
 
-void kernel_main() {
-    print_clear();
-    print_set_color(PRINT_COLOR_GREEN, PRINT_COLOR_BLACK);
-    print_str("Welcome to Generation OS!");
-    print_str("This OS is still in development");
-}
+    void kernel_main() {
+        print_clear();
+        print_set_color(PRINT_COLOR_GREEN, PRINT_COLOR_BLACK);
+        print_str("Welcome to Generation OS!");
+        print_newline();
+        print_str("This OS is still in development");
+        print_newline();
+        
+        while (0 == 0){
+            print_char(port_byte_in(0x60));
+        }
+            
+    }
